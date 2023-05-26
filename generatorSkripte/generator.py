1e0b3f42917a30c6c68f328318e486b60a63a559
zcerne <76513572+zcerne@users.noreply.github.com>
2023-05-26 11:00:23 +0200

    Osnoovne stvari za skripto delajo.

--- generatorSkripte/generator.py
+++ generatorSkripte/generator.py
@@ -1,19 +1,24 @@
 import numpy as np
 import json
 import re
+import flatdict
 
-def izpisiLanguageStrings(lst:list):
-    slv = {}
-    for par in lst:
-        slv.update(par)
+def izpisiLanguageStrings():
+    global slvLS
     pySlv = {"languageStrings":{"sl":{}}}
-
-    pySlv["languageStrings"]["sl"] = slv
+    pySlv["languageStrings"]["sl"] = slvLS
     
     # jsonStr = json.dumps(pySlv, indent = 5, ensure_ascii=False)
     # jsString = re.sub("\"([^\"]+)\":", r"\1:", jsonStr).replace(r'\"', "")
     # return jsString[1:-1]
     return pySlv
+
+def dodajSlovar(slv1, slv2):
+    f1 = flatdict.FlatDict(slv1)
+    f2 = flatdict.FlatDict(slv2)
+
+    f1[f2.keys()[0]] = f2.values()[0]
+    return f1.as_dict()
 
 def izpisiHideControls(restart = False, saveOrLoad = False, loadBestAnswer = False, speedSlider = False, backToFirst = False, nextStep = False, goToEnd = False):
     pySlv = {"hideControls":{}}
@@ -39,25 +44,53 @@ def izpisiRandomBulshit1(introMaxHeight = "33%", maxListSize = 100, scrollbars =
         }
     return pySlv
 
-def izpisiStartingExample(str):
+def izpisiStartingExample():
+    global strSE
     pySlv = {"startingExample":{}}
     
     #python slovar
-    pySlv["startingExample"]["blockly"] = str
+    pySlv["startingExample"]["blockly"] = strSE
     # jsonStr = json.dumps(pySlv, indent = 5)
     # jsString = re.sub("\"([^\"]+)\":", r"\1:", jsonStr).replace(r'\"', "")
     # return jsString[1:-1]
     return pySlv
 
-def izpisiIncludeBlocks(gbc = True, robot = ["move"], iclAll = True, whCat = [], sinBl = [], excBl = []):
-    slv = {"groupByCategory": gbc, "generatedBlocks": {"robot": robot}, "standardBlocks": {"includeAll": iclAll, "wholeCategories": whCat, "singleBlocks": sinBl, "excludedBlocks": excBl,},}
+def izpisiIncludeBlocks():
+    global groupByCategory, includeAllIB, wholeCategories, robotIB, singleBlocksIB, excludedBlocksIB
+    
+    robotList = []
+    wholeCategoriesList = []
+    for key in robotIB.keys():
+        if robotIB[key]:
+            robotList.append(key)
+    for key in wholeCategories.keys():
+        if wholeCategories[key]:
+            wholeCategoriesList.append(key)
+
+    slv = {"groupByCategory": groupByCategory, "generatedBlocks": {"robot": robotList}, "standardBlocks": {"includeAll": includeAllIB, "wholeCategories": wholeCategoriesList, "singleBlocks": singleBlocksIB, "excludedBlocks": excludedBlocksIB,},}
     pySlv = {"includeBlocks":{}}
     
     pySlv["includeBlocks"] = slv
     
     return pySlv
 
-def izpisiCheckEndCondition(indikator1 = "category", ime1 = "coin", negIndikator1 = "", negIme1 = "", indikatorA = "", imeA = "", indikatorB = "", imeB = "", keys = "", negIndikatorA = "", negImeA = "", negIndikatorB = "", negImeB = ""):
+def izpisiCheckEndCondition():
+    global endCondition
+
+    indikator1 = endCondition["Exist"]["indikator1"]
+    ime1 = endCondition["Exist"]["ime1"]
+    negIndikator1 = endCondition["Exist"]["negIndikator1"]
+    negIme1 = endCondition["Exist"]["negIme1"]
+    indikatorA = endCondition["Coincide"]["indikatorA"]
+    imeA = endCondition["Coincide"]["imeA"]
+    indikatorB = endCondition["Coincide"]["indikatorB"]
+    imeB = endCondition["Coincide"]["imeB"]
+    keys = endCondition["Coincide"]["keys"]
+    negIndikatorA = endCondition["Coincide"]["negIndikatorA"]
+    negImeA = endCondition["Coincide"]["negImeA"]
+    negIndikatorB = endCondition["Coincide"]["negIndikatorB"]
+    negImeB = endCondition["Coincide"]["negImeB"]
+
     lst = [indikator1, ime1, negIndikator1, negIme1, indikatorA, imeA, indikatorB, imeB, keys, negIndikatorA, negImeA, negIndikatorB, negImeB]
     
     filters1 = r"{}"
@@ -110,113 +143,134 @@ def izpisiRandomBulsit2(border = 0, backgroundColour = "white", backgroundTile =
     "numberOfRobots": numberOfRobots}
     return slv
 
-def izpisiItemTypes(slv: dict):
+def izpisiItemTypes():
+    global itemsIT
     pySlv = {"itemTypes":{}}
-    pySlv["itemTypes"] = slv
-
+    pySlv["itemTypes"] = itemsIT
     return pySlv
 
-def ustvariItemType(num, img = "", zOrder = 0, nbStates = 1, category = "", value = 0):
-    slv = {}
+def dodajItemType(itemSpecifications):
+    global itemsIT, itemID, typeOptions, possibleCategories
+    cats = itemSpecifications["category"]
+    catsTrue = []
+    for cat in cats.keys():
+        if cats[cat]:
+            catsTrue.append({"\"%s\""%cat:True})
+            possibleCategories.add(cat)
     
-    slv["num"] = num
-    slv["img"] = img
-    slv["zOrder"] = zOrder
-    slv["nbStates"] = nbStates
-    slv["category"] = category
-    slv["value"] = value
-
-    return slv
-
-def izpisiSubTaskData(lst: list):
-    pySlv = {"easy": lst}
-
-    return pySlv
+    itemSpecifications["category"] = catsTrue
+    ime = itemSpecifications.pop("name")
+    itemsIT[ime] = itemSpecifications
+    typeOptions.append(ime)
     
 
-def sestaviMatriko(n,m):
-    return "&#&" + np.array_repr(np.ones((n,m), dtype = np.int8)).replace("array(", "").replace(", dtype=int8)", "") + "&#&"
+def izpisiSubTaskData():
+    global matrixExamples, initialisationExamples
+    pyLst = []
 
-def sestaviIncializacijo(row, col, type, dir = 0, value = 0):
-    slv = {}
+    for i in range(len(initialisationExamples)):
+        pyLst.append({"tiles":izpisiMatriko(matrixExamples[i]), "initItems":initialisationExamples[i]})
+
+    return pyLst
     
-    slv["row"] = row
-    slv["col"] = col
-    slv["dir"] = dir
-    slv["type"] = type
-    slv["value"] = value
+def izpisiMatriko(matrix):
+    return "&#&" + np.array_repr(matrix).replace("array(", "").replace(", dtype=int8)", "") + "&#&"
 
-    return slv
 
-#PARAMETRI
+def addMatrix(mmm, nnn):
+    global matrixExamples, aktivenPrimer
+    matrixExamples[aktivenPrimer] = np.ones((nnn,mmm), dtype = np.int8)
 
+def addInicialisation():
+    global initialisationExamples, aktivenPrimer, inicialisationOptions
+    initialisationExamples[aktivenPrimer].append(inicialisationOptions)
+
+def addExample():
+    global initialisationExamples, aktivenPrimer, mmm, nnn
+    aktivenPrimer += 1
+    initialisationExamples.append([])
+    matrixExamples.append([])
+    addMatrix(mmm, nnn)
+
+
+
+#Globalna spremenljivka LANGUAGE STRINGS - shranjuje vse language stringe
 slvLS = {}
-s1LS = "program 2"
-par1LS = {"startingBlockName": s1LS,} #zadnji vnos je poljuben, ostali so fiksni
-par2LS = {"categories": {"actions": "Gibanje"}}
-par3LS = {"label": {"changeRobot": "zamenjaj vlogo %1 HAHA"}}
-par4LS = {"label": {"colour": "LOL %1",}}
-par5LS = {"options":{"pick": "poberi UPS",}}
-par6LS = {"messages":{"itemsExist": "Kovanci so na mreži."}}
-par7LS = {"messages":{"itemsDontExist": "Kovancev ni na mreži."}}
-par8LS = {"options":{"tools": {"bool": {"category": "je"}}}}
-parsLS = [par1LS, par2LS, par3LS, par4LS, par5LS, par6LS, par7LS, par8LS]
+#posodobitev slvLS
+parLS = {"categories": {"actions": "Gibanje"}} #dobljeni parameter s spletne strani, možne parametre najdeš v templateText.txt
+slvLS = dodajSlovar(slvLS, parLS) # za posodobitev kliči to funkcijo in za drugi paramter uporabi kar pride iz spletne strani
 
-strSE = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="robot_start" id="Yx#}`-PvOO]HA4c0m7]F" deletable="false" movable="false" editable="false" x="0" y="0"><next><block type="move" id="CoO|[q[@qpAx1_*bS`Or"><field name="PARAM_0">S</field><value name="PARAM_1"><shadow type="math_number" id="Hyc?21Pdl93/lRnSQ;yk"><field name="NUM">5</field></shadow></value><next><block type="move" id=".0,k.*t.-0!PhL2?iW=S"><field name="PARAM_0">W</field><value name="PARAM_1"><shadow type="math_number" id="Eoy,Bt|qklJL]F[A_HUS"><field name="NUM">5</field></shadow></value><next><block type="move" id="-0w[|/Bnh{6lzt*/qE/1"><field name="PARAM_0">N</field><value name="PARAM_1"><shadow type="math_number" id=")T;FmigiJI@U(YA2/T+["><field name="NUM">1</field></shadow></value></block></next></block></next></block></next></block><block type="controls_repeat_ext" id="2E]Uhs@W}KrbH3PJF36*" x="20" y="155"><value name="TIMES"><shadow type="math_number" id="z_GWSb[:HQjB|Qf]0C9N"><field name="NUM">3</field></shadow></value><statement name="DO"><block type="moveSimple" id="yar~u.Qkhzs]q=@VlZ9c"><field name="PARAM_0">S</field></block></statement><next><block type="controls_repeat_ext" id="z@Gfs[CGcB6T{G-72W|z"><value name="TIMES"><shadow type="math_number" id="73LZY(lJ1blr[EH[s;W*"><field name="NUM">4</field></shadow></value><statement name="DO"><block type="moveSimple" id="#-0m.#:qs#=w*PlCX[qW"><field name="PARAM_0">W</field></block></statement></block></next></block><additional>{}</additional></xml>'
+# ZAČETNA POSTAVITEV - na spletni strani naj bo gumb - posodobi začetno postavitev. Samo zamenjaj string
+strSE = ''
 
-robotIB = ["move", "moveSimple", "forward", "forwardSimple", "turn", "turnAround", "jump", "changeRobot", "transport", "sensorBool", "sensorValue", "alterValue", "destroy", "create", "wait", "nitems", "sensorRowCol",]
+
+# INCLUDE BLOCKS
+groupByCategory = True # neki za obklukat
+includeAllIB = False # neki za obklukat
+wholeCategories = {"tools":False, "logic":False, "loops":False, "math":False, "texts":False, "lists":False, "colour":False, "variables":False, "functions":False}
+# bloki za robota, na začetku so vsi false, na spletni strani naj bo dropdown za klukat, ko obkljuka spremeni v True
+robotIB = {"move": False, "moveSimple": False, "forward": False, "forwardSimple": False, "turn": False, "turnAround": False, "jump": False, "changeRobot": False, "transport": False, "sensorBool": False, "sensorValue": False, "alterValue": False, "destroy": False, "create": False, "wait": False, "nitems": False, "sensorRowCol": False}
+singleBlocksIB = {}
+excludedBlocksIB = {}
+# moćžnosti "move", "moveSimple", "forward", "forwardSimple", "turn", "turnAround", "jump", "changeRobot", "transport", "sensorBool", "sensorValue", "alterValue", "destroy", "create", "wait", "nitems", "sensorRowCol"
+
+# END CONDITIONS
+possibleIdicateors = {'category', 'value', 'type'}
+possibleCategories = {} # updejta se pri ustvarjenju objektov
+typeOptions = [] #imena predmetov, ki so na izbiro za inicializacijo objekta, dodajajo se avtomatsko s klicanjem funkcije dodajItemType.
+# opcije za indikator = possibleIdicateors
+# odvisno od izbranega indikatorja so odvisna tudi imena
+# če je indikator category = opcije za ime = possibleCategories
+# če je indikator value = opcije za ime = kerakol številka. Mejbi bo treba dtr omejitev
+# če je indikator type = opcije za ime = typeOptions
+# jebeš keys, nerabš
+# ubistvu lahko karkol napišeš notr, prazne opcije bo pohendlala funkcija
+endCondition = {"Exist": {"indikator1": "category", "ime1": "coin", "negIndikator1": "", "negIme1": ""},
+                "Coincide": {"indikatorA": "", "imeA": "", "indikatorB": "", "imeB": "", "keys": "", "negIndikatorA": "", "negImeA": "", "negIndikatorB": "", "negImeB": ""}}
+
 
 #OBJEKTI
-ime1IT = "robot0"
-numIT = 2
-imgIT = ["pisek.png"]
-zOrderIT = 2
-nbStatesIT = 8
-catIT = "robot"
-categoryIT = [{"\"%s\""%catIT:True}]
-item1IT = ustvariItemType(numIT, imgIT, zOrderIT, nbStatesIT, categoryIT)
+itemsIT = {} #shranjuje vse iteme
+itemID = 2 #se poveča samodejno, odvisen od števila itemov
+catIT = {'junak': False, 'ovira': False, 'paket': False, 'gumb': False, 'kovanec': False, 'številka': False} #za obklukat
 
-slvIT = {}
-slvIT[ime1IT] = item1IT
+# imgIT = ["pisek.png"]
+# zOrderIT #naj bo odvisen od vrstnega reda stvaritve objektov, seprav isti kokritemID
+# nbStatesIT = 8 odvisen le od robota
+
+itemSpecifications = {"name":"objekt_{}".format(itemID-2), "num": itemID, "img":"", "zOrder":itemID, "category":catIT, "value":0}
+dodajItemType(itemSpecifications) #kliče naj se z gumbom ustvari
 
 #MREŽA
-matrika1TD = sestaviMatriko(4,5)
-items1TD = [sestaviIncializacijo(row = 2, col = 2, dir = 4, type = "robot", value = 1), sestaviIncializacijo(row = 3, col = 1, dir = 4, type = "krNekej", value = 5)]
-easy1TD = {"tiles":matrika1TD, "initItems":items1TD}
+#GLOBAL
+aktivenPrimer = 0
+mmm = 5
+nnn = 5
+matrixExamples = [[]] #seznam matrik - lahko da je več testov
+initialisationExamples = [[]]
+ityp = 0 # izberi iz typeOptions
+inicialisationOptions = {"row":0, "col":0, "type":typeOptions[ityp], "dir": 0, "value": 0}
+addMatrix(mmm, nnn) #dodaj test, naj se izvede ob zagonu
+addInicialisation() # kliči da dodaš ityem v initialisationExamples
+addExample() #kliči če želiš dodati dodaten primer
 
-#INICIALIZACIJA
-matrika2TD = sestaviMatriko(5,8)
-items2TD = [sestaviIncializacijo(row = 2, col = 2, dir = 4, type = "robot", value = 1), sestaviIncializacijo(row = 3, col = 1, dir = 4, type = "krNekej", value = 5)]
-easy2TD = {"tiles":matrika2TD, "initItems":items2TD}
-
-lstTD = [easy1TD, easy2TD]
-
-languageStrings = izpisiLanguageStrings(slvLS)
-hideControls = izpisiHideControls()
-randomBulshit1 = izpisiRandomBulshit1()
-startingExample = izpisiStartingExample(strSE)
-includeBlocks = izpisiIncludeBlocks(robot = robotIB)
-checkEndCondition = izpisiCheckEndCondition()
-randomBulsit2 = izpisiRandomBulsit2()
-itemTypes = izpisiItemTypes(slvIT)
-subTaskData = izpisiSubTaskData(lstTD)
-
-def zdruzi(languageStrings, hideControls, includeBlocks, startingExample, checkEndCondition, itemTypes, subTaskData):
+def ustvariSkripto():
     ulmSlv = {}
-    ulmSlv.update(languageStrings)
-    ulmSlv.update(hideControls)
-    ulmSlv.update(randomBulshit1)
-    ulmSlv.update(includeBlocks)
-    ulmSlv.update(startingExample)
-    ulmSlv.update(checkEndCondition)
-    ulmSlv.update(randomBulsit2)
-    ulmSlv.update(itemTypes)
+    ulmSlv.update(izpisiLanguageStrings())
+    ulmSlv.update(izpisiHideControls())
+    ulmSlv.update(izpisiRandomBulshit1())
+    ulmSlv.update(izpisiIncludeBlocks())
+    ulmSlv.update(izpisiStartingExample())
+    ulmSlv.update(izpisiCheckEndCondition())
+    ulmSlv.update(izpisiRandomBulsit2())
+    ulmSlv.update(izpisiItemTypes())
 
     jsonStr = json.dumps(ulmSlv, indent = 5, ensure_ascii=False)
     jsString1 = re.sub("\"([^\"]+)\":", r"\1:", jsonStr).replace(r'\"', "")
     jsString1 = jsString1.replace("\"&#&", "").replace("&#&\"", "").replace("\\\\", "\"").replace("\\n", "\n").replace("\\t", "\t")
     str1 = "subTask.gridInfos = {};".format(jsString1)
 
+    subTaskData = izpisiSubTaskData()
     jsonStr = json.dumps(subTaskData, indent = 5, ensure_ascii=False)
     jsString2 = re.sub("\"([^\"]+)\":", r"\1:", jsonStr).replace(r'\"', "").replace("\"&#&", "").replace("&#&\"", "")
     jsString2 = jsString2.replace("\\n", "\n")
@@ -227,4 +281,4 @@ def zdruzi(languageStrings, hideControls, includeBlocks, startingExample, checkE
     fajl.write(theString)
     fajl.close()
 
-zdruzi(languageStrings, hideControls, includeBlocks, startingExample, checkEndCondition, itemTypes, subTaskData)
+ustvariSkripto()
